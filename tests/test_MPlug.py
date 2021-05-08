fd5b5dca5b68fb30033c24d395271cfc308cc23b
Ryan Porter <ryan.m.porter@gmail.com>
2021-05-08 12:15:01 -0700

    Implmenet MPlug::array

--- tests/test_MPlug.py
+++ tests/test_MPlug.py
@@ -1,1 +1,47 @@
+"""Test suite for MPlug bindings."""
+
+import nose.tools
+
+from maya import cmds 
+
 import cmdc
+
+def test_plug_array_pass():
+    node = cmds.createNode('network')
+
+    cmds.addAttr(node, ln='test', multi=True)
+
+    attr_name = '{}.test'.format(node)
+    elem_name = '{}[0]'.format(attr_name)
+
+    cmds.setAttr(elem_name, 0.0)
+
+    array_element = cmdc.SelectionList().add(elem_name).getPlug(0)
+    array_root = array_element.array()
+
+    assert array_root is not None
+    assert array_root.name() == attr_name
+
+
+def test_plug_array_fails_on_non_array_plug():
+    node = cmds.createNode('network')
+
+    cmds.addAttr(node, ln='test')
+
+    attr_name = '{}.test'.format(node)
+
+    array_element = cmdc.SelectionList().add(attr_name).getPlug(0)
+
+    nose.tools.assert_raises(
+        TypeError,
+        array_element.array
+    )
+
+    
+def test_plug_array_fails_on_null_plug():
+    plug = cmdc.Plug()
+    
+    nose.tools.assert_raises(
+        ValueError,
+        plug.array
+    )
