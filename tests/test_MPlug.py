837d6030199016651065e0b4540f9b22eba7ae9b
Ryan Porter <ryan.m.porter@gmail.com>
2021-05-08 12:18:05 -0700

    Refactor test suite to group by method

--- tests/test_MPlug.py
+++ tests/test_MPlug.py
@@ -6,42 +6,44 @@ from maya import cmds
 
 import cmdc
 
-def test_plug_array_pass():
-    node = cmds.createNode('network')
+class test_array(object):
+    @staticmethod
+    def test_pass():
+        node = cmds.createNode('network')
 
-    cmds.addAttr(node, ln='test', multi=True)
+        cmds.addAttr(node, ln='test', multi=True)
 
-    attr_name = '{}.test'.format(node)
-    elem_name = '{}[0]'.format(attr_name)
+        attr_name = '{}.test'.format(node)
+        elem_name = '{}[0]'.format(attr_name)
 
-    cmds.setAttr(elem_name, 0.0)
+        cmds.setAttr(elem_name, 0.0)
 
-    array_element = cmdc.SelectionList().add(elem_name).getPlug(0)
-    array_root = array_element.array()
+        array_element = cmdc.SelectionList().add(elem_name).getPlug(0)
+        array_root = array_element.array()
 
-    assert array_root is not None
-    assert array_root.name() == attr_name
+        assert array_root is not None
+        assert array_root.name() == attr_name
 
+    @staticmethod
+    def test_fails_on_non_array_plug():
+        node = cmds.createNode('network')
 
-def test_plug_array_fails_on_non_array_plug():
-    node = cmds.createNode('network')
+        cmds.addAttr(node, ln='test')
 
-    cmds.addAttr(node, ln='test')
+        attr_name = '{}.test'.format(node)
 
-    attr_name = '{}.test'.format(node)
+        array_element = cmdc.SelectionList().add(attr_name).getPlug(0)
 
-    array_element = cmdc.SelectionList().add(attr_name).getPlug(0)
+        nose.tools.assert_raises(
+            TypeError,
+            array_element.array
+        )
 
-    nose.tools.assert_raises(
-        TypeError,
-        array_element.array
-    )
-
-    
-def test_plug_array_fails_on_null_plug():
-    plug = cmdc.Plug()
-    
-    nose.tools.assert_raises(
-        ValueError,
-        plug.array
-    )
+    @staticmethod
+    def test_fails_on_null_plug():
+        plug = cmdc.Plug()
+        
+        nose.tools.assert_raises(
+            ValueError,
+            plug.array
+        )
