80ee3d0728cf282d550a7f78aab286d33ffd1780
Ryan Porter <ryan.m.porter@gmail.com>
2021-06-12 11:51:34 -0700

    Update docker build linux script to use maya{version} environment

--- docker_build_linux.ps1
+++ docker_build_linux.ps1
@@ -1,9 +1,8 @@
 param (
-    [string]$maya_version = "2020"
+    [string]$maya_version = "2022"
 )
 
 docker run -ti --rm `
-    -v ${env:DEVKIT_LOCATION}:/devkit `
     -v ${PWD}:/workspace `
-    -e DEVKIT_LOCATION=/devkit `
-    cmdc ./build_linux.sh $maya_version
+    cmdc:${maya_version} `
+     ./build_linux.sh $maya_version
