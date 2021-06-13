744a4e2c979e52feb5428921735519d12768ce4c
Loïc Pinsard <muream@gmail.com>
2021-06-13 17:49:39 +0200

    fail stubs generation in case of degraded signatures.

--- scripts/generate_stubs.py
+++ scripts/generate_stubs.py
@@ -1,10 +1,16 @@
-import cmdc
-
 import inspect
-import pybind11_stubgen
+import sys
 import time
 
+import cmdc
+import pybind11_stubgen
+
 STUBS_LOCATION = "build/cmdc.pyi"
+
+
+class InvalidSignatureError(Exception):
+    """Raised when one or more signatures are invalid."""
+
 
 def cleanup_imports(content):
     """Remove any classes accidentally imported as modules.
@@ -29,36 +35,48 @@ def cleanup_imports(content):
 
     return content
 
-print("Generating stubs")
-t0 = time.time()
 
-module = pybind11_stubgen.ModuleStubsGenerator(cmdc)
-module.write_setup_py = False
+def main():
+    print("Generating stubs")
+    t0 = time.time()
 
-print("(1) Parsing module..")
+    module = pybind11_stubgen.ModuleStubsGenerator(cmdc)
+    module.write_setup_py = False
 
-module.parse()
+    print("(1) Parsing module..")
 
-t1 = time.time()
-print("(1) Finished in {0:0.3} s".format(t1-t0))
-print("(1) ----------------------------")
+    module.parse()
 
-print("(2) Generating stubs content..")
+    invalid_signatures_count = pybind11_stubgen.FunctionSignature.n_invalid_signatures
+    if invalid_signatures_count > 0:
+        raise InvalidSignatureError(
+            f"Module contains {invalid_signatures_count} invalid signature(s)"
+        )
 
-content = "\n".join(module.to_lines())
-content = cleanup_imports(content)
+    t1 = time.time()
+    print("(1) Finished in {0:0.3} s".format(t1 - t0))
+    print("(1) ----------------------------")
 
-t2 = time.time()
-print("(2) Finished in {0:0.3} s".format(t2-t1))
-print("(2) ----------------------------")
-print("(3) Writing stubs file..")
+    print("(2) Generating stubs content..")
 
-with open(STUBS_LOCATION, "w") as handle:
-    handle.write(content)
+    content = "\n".join(module.to_lines())
+    content = cleanup_imports(content)
 
-t3 = time.time()
-print("(3) Finished in {0:0.3} s".format(t3-t2))
-print("(3) ----------------------------")
+    t2 = time.time()
+    print(f"(2) Finished in {t2 - t1:0.3} s")
+    print("f(2) ----------------------------")
 
-print("Succesfully created .{0} in {1:0.3} s".format(STUBS_LOCATION, t3-t0))
+    print("(3) Writing stubs file..")
 
+    with open(STUBS_LOCATION, "w") as handle:
+        handle.write(content)
+
+    t3 = time.time()
+    print(f"(3) Finished in {t3 - t2:0.3} s")
+    print("(3) ----------------------------")
+
+    print(f"Succesfully created .{STUBS_LOCATION} in {t3 - t0:0.3} s")
+
+
+if __name__ == "__main__":
+    main()
