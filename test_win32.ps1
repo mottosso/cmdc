a3d850cadd68957bb65c43bcd78f409f6ca473dd
Scott Englert <scott@scottenglert.com>
2022-04-13 10:59:22 -0700

    Delete test_win32.ps1

--- test_win32.ps1
+++
@@ -1,9 +0,0 @@
-$env:PYTHONDONTWRITEBYTECODE="1"
-
-if (-not $args.count) {
- $directory = "./tests"
-} else {
- $directory = $args[0]
-}
-
-."$env:MAYA_LOCATION/bin/mayapy.exe" -B -m nose -xv $directory
