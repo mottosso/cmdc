3731ff43134b1c184211492e1fe8ca77aff75634
Scott Englert <scott@scottenglert.com>
2022-09-27 22:54:24 -0700

    removing mac build

--- build_mac/CMakeFiles/BuildMFnTypes.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/BuildMFnTypes"
-  "src/MFn.Types.inl"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BuildMFnTypes.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build_mac/CMakeFiles/cmdc.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cmdc.
-# This may be replaced when dependencies are built.
